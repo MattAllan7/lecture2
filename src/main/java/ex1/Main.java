4afff8049ad8db0af47ca5d9a8d6ee95bf9b5582
Matt Allan <mattallan314@gmail.com>
2023-02-22 09:20:28 -0500

    1

---
+++ src/main/java/ex1/Main.java
@@ -0,0 +1,30 @@
+package ex1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //bits describe the size of the CPU's data bus
+
+        int piI = 3;
+        double piD = 3.14;
+        float piF = 3.14f;
+
+        /*
+        typeName variableName = value;
+                    OR
+        typeName variableName;
+         */
+
+        String greeting = "Hello, Dave";
+
+        String name = "Matt "+"Allan "+"was born in "+ 2005;
+            //number (2005) is converted into a string by the compiler
+
+        //typing "so" and pressing TAB is the shortcut for System.out.println();
+
+        //camel case
+
+
+    }
+}
