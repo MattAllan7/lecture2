5f6aefd0ffb9fca7c8529c98326e437306aee888
Matt Allan <mattallan314@gmail.com>
2023-02-22 09:44:21 -0500

    2

--- src/main/java/ex1/Main.java
+++ src/main/java/ex1/Main.java
@@ -1,8 +1,8 @@
 package ex1;
 
-public class Main
+public class Main //class
 {
-    public static void main(String[] args)
+    public static void main(String[] args) //method
     {
         //bits describe the size of the CPU's data bus
 
@@ -10,10 +10,10 @@ public class Main
         double piD = 3.14;
         float piF = 3.14f;
 
-        /*
-        typeName variableName = value;
-                    OR
-        typeName variableName;
+        /**
+        * typeName variableName = value;
+        *             OR
+        * typeName variableName;
          */
 
         String greeting = "Hello, Dave";
@@ -23,10 +23,32 @@ public class Main
 
         //typing "so" and pressing TAB is the shortcut for System.out.println();
 
-        //camel case
+        /**
+         * camel case
+         * cannot name a variable as a variable type
+         * e.g. double int = 1.1;
+         * cannot start the name of a variable with a number
+         */
 
         //commit: saves repository changes  on local
         //push: updates git commit changes and sends it to remote repo (GitHub)
 
+        double unitPrice = 1.95;
+        int quantity = 2;
+
+        System.out.print("Total price: ");
+        System.out.println(unitPrice * quantity);
+
+        /**
+         * calling methods:
+         */
+
+        int sum = add(12,5);
+
+    }
+    private static int add(int x, int y)
+    {
+        int sum = x + y;
+        return sum;
     }
 }
